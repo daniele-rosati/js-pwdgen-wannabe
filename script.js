fef7028ab32ff7f429f0407898dbe3952bb046e5
daniele-rosati <drosati151@gmail.com>
2024-03-04 20:35:15 +0100

    password

---
+++ script.js
@@ -0,0 +1,19 @@
+// chiedo all'utente il nome 
+
+const userName = prompt('dimmi il tuo nome');
+
+// chiedo all'utente il cognome
+
+const userSurname = prompt('dimmi il tuo cognome');
+
+// chiedo all'utente il colore preferito
+
+const userFavcolor = prompt('dimmi il tuo colore preferito')
+
+// somma delle variabili
+
+let sum = userName + userSurname + userFavcolor
+
+// generazione password
+
+const password = 'la tua password è: ' + sum
